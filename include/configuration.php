805856344dbe24af768cb7c82588277215fc0db5
Joel Wan <joelwan@gmail.com>
2011-12-17 12:44:39 -0800

    change version number

--- include/configuration.php
+++ include/configuration.php
@@ -4,7 +4,7 @@ $configuration['soapEngine'] = "nusoap"; //other value is "phpsoap"
 $configuration['soap'] = "http://beta.phpobjectgenerator.com/services/soap.php?wsdl";
 $configuration['homepage'] = "http://beta.phpobjectgenerator.com";
 $configuration['revisionNumber']="";
-$configuration['versionNumber'] = "3.0f";
+$configuration['versionNumber'] = "3.1beta";
 $configuration['author'] = "Php Object Generator";
 $configuration['copyright'] = "Free for personal & commercial use. (Offered under the BSD license)";
 ?>
