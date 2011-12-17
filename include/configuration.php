d1968be42dae18cdf4e9948cdf31d9de7656382f
Joel Wan <joelwan@gmail.com>
2011-12-17 12:43:40 -0800

    point github repo to beta site

--- include/configuration.php
+++ include/configuration.php
@@ -1,8 +1,8 @@
 <?php
 global $configuration;
 $configuration['soapEngine'] = "nusoap"; //other value is "phpsoap"
-$configuration['soap'] = "http://www.phpobjectgenerator.com/services/soap.php?wsdl";
-$configuration['homepage'] = "http://www.phpobjectgenerator.com";
+$configuration['soap'] = "http://beta.phpobjectgenerator.com/services/soap.php?wsdl";
+$configuration['homepage'] = "http://beta.phpobjectgenerator.com";
 $configuration['revisionNumber']="";
 $configuration['versionNumber'] = "3.0f";
 $configuration['author'] = "Php Object Generator";
