500128935f80173694d8df98fca5aaff84108dd8
Joel Wan <joelwan@gmail.com>
2013-02-02 16:09:54 -0800

    consume web service from correct subdomain

--- include/configuration.php
+++ include/configuration.php
@@ -1,8 +1,8 @@
 <?php
 global $configuration;
 $configuration['soapEngine'] = "phpsoap"; //other value is "phpsoap"
-$configuration['soap'] = "http://beta.phpobjectgenerator.com/services/soap.php?wsdl";
-$configuration['homepage'] = "http://beta.phpobjectgenerator.com";
+$configuration['soap'] = "http://www.phpobjectgenerator.com/services/soap.php?wsdl";
+$configuration['homepage'] = "http://www.phpobjectgenerator.com";
 $configuration['revisionNumber']="";
 $configuration['versionNumber'] = "3.1beta";
 $configuration['author'] = "Php Object Generator";
